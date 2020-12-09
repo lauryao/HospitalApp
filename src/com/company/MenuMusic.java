f1d9b61b7959780b5db4c0c375486ac56faabff8
Infernaton <lubin.pauly@gmail.com>
2020-12-09 16:30:35 +0100

    Merge remote-tracking branch 'origin/master' V2

--- src/com/company/MenuMusic.java
+++ src/com/company/MenuMusic.java
@@ -8,23 +8,21 @@ public class MenuMusic {
         boolean start = true;
 
         while(start) {
-            System.out.print("\nWhat do you want to do?"
-                    + "\n1- Add a song"
-                    + "\n2- View a playlist"
-                    + "\n3- Delete a song"
-                    + "\n4- Delete a playlist"
-                    + "\n5- Exit"
-                    + "\nSelection: ");
+            System.out.print("""
+
+                    What do you want to do?
+                    1 - Song
+                    2 - Playlist
+                    3 - Exit
+                    Selection:\s""");
 
             Scanner scan = new Scanner(System.in);
             String selectOption = scan.nextLine();
 
             switch ((selectOption)) {
-                case "1" -> musicDescription();
-                case "2" -> System.out.println("2");
-                case "3" -> System.out.println("3");
-                case "4" -> System.out.println("4");
-                case "5" -> System.exit(0);
+                case "Song" -> switchSong();
+                case "Playlist" -> System.out.println("2");
+                case "Exit" -> start= false;
             }
             System.out.print("\n");
         }
@@ -44,6 +42,46 @@ public class MenuMusic {
         String time= scanTime.nextLine();
 
         Music music = new Music(name,author,time);
+
+    }
+    public void switchSong() {
+
+        Scanner scanSwitchSong = new Scanner(System.in);
+
+        System.out.print("""
+
+                What do you want to do?
+                1- Add a song
+                2- Delete a song
+                3- Listen a song
+                4- Go back"
+                5- Exit"
+                Selection:\s""");
+        String switchSong = scanSwitchSong.nextLine();
+        switch ((switchSong)) {
+            case "1" -> musicDescription();
+            case "2" -> System.out.println("2");
+            case "3" -> System.out.println("3");
+            case "4" -> { break; }
+            case "5" -> System.exit(0);
+        }
+
+    }
+    public void switchPlaylist() {
+        Scanner scanSwitchPlaylist = new Scanner(System.in);
+
+        System.out.print("\nWhat do you want to do?"
+                + "\n1- Add a playlist"
+                + "\n2- Delete a playlist"
+                + "\n3- Listen a playlist"
+                + "\n4- Go back"
+                + "\n5- Exit"
+                + "\nSelection: ");
+        String switchSong = scanSwitchPlaylist.nextLine();
+        switch ((switchSong)) {
+            case "4" -> { break; }
+            case "5" -> System.exit(0);
+        }
 
     }
 }
