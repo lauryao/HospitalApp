8444368f86846e325e9e2bc70806e6b3d29e4a90
Thomas Cardoso <72390135+tcardo06@users.noreply.github.com>
2020-12-09 14:15:08 +0100

    global rearrangement

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -17,16 +17,18 @@ public class Menu {
 
             Scanner scan = new Scanner(System.in);
             String selectOption = scan.nextLine();
+            
             switch ((Scanner)) {
                 case "1" -> musicDescription();
                 case "2" -> System.out.println("2");
                 case "3" -> System.out.println("3");
                 case "4" -> System.out.println("4");
                 case "5" -> System.exit(0);
-        }
-        System.out.print("\n");
-        }
+            }
+            System.out.print("\n");
+       }
     }
+    
         public void musicDescription(){
         Scanner scanName = new Scanner(System.in);
         System.out.println("Enter the music name : ");
@@ -41,5 +43,6 @@ public class Menu {
         String time= scanTime.nextLine();
 
         Music music = new Music(name,author,time);
+            
     }
 }
