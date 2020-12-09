05178be98c70c00d0f1406b34d4c5a70056d537c
Thomas Cardoso <72390135+tcardo06@users.noreply.github.com>
2020-12-09 14:02:01 +0100

    rename options scanner + readjustment of options

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -7,19 +7,22 @@ public class Menu {
         boolean start = true;
         
         while(start) {
-        System.out.print("\nQue voulez faire?"
-                + "\n1- Ajouter une chanson"+ "\n2- Voir une playlist"
-                + "\n3- Supprimer une chanson" + "\n4- Supprimer une playlist"
-                + " \n5- Quitter" + "\nSelection: ");
+            System.out.print("\nQue voulez faire?"
+                    + "\n1- Ajouter une chanson"
+                    + "\n2- Voir une playlist"
+                    + "\n3- Supprimer une chanson"
+                    + "\n4- Supprimer une playlist"
+                    + "\n5- Quitter"
+                    + "\nSéléction: ");
 
-        Scanner scan = new Scanner(System.in);
-        String Scanner = scan.nextLine();
-        switch ((Scanner)) {
-            case "1" -> System.out.println("1");
-            case "2" -> musicDescription();
-            case "3" -> System.out.println("3");
-            case "4" -> System.out.println("4");
-            case "5" -> System.exit(0);
+            Scanner scan = new Scanner(System.in);
+            String selectOption = scan.nextLine();
+            switch ((Scanner)) {
+                case "1" -> musicDescription();
+                case "2" -> System.out.println("2");
+                case "3" -> System.out.println("3");
+                case "4" -> System.out.println("4");
+                case "5" -> System.exit(0);
         }
         System.out.print("\n");
         }
