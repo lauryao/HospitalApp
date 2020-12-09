fde9c38fd4ae0427e7013980c7df358ded3f3757
Thomas Cardoso <72390135+tcardo06@users.noreply.github.com>
2020-12-09 13:56:01 +0100

    add music + exit function add + loop run program
    function to add music + function to exit the program + loop to run program

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -4,22 +4,39 @@ import java.util.Scanner;
 
 public class Menu {
     public Menu() {
-        Scanner scan = new Scanner(System.in);
-
+        boolean start = true;
+        
+        while(start) {
         System.out.print("\nQue voulez faire?"
                 + "\n1- Ajouter une chanson"+ "\n2- Voir une playlist"
                 + "\n3- Supprimer une chanson" + "\n4- Supprimer une playlist"
                 + " \n5- Quitter" + "\nSelection: ");
 
+        Scanner scan = new Scanner(System.in);
         String Scanner = scan.nextLine();
         switch ((Scanner)) {
             case "1" -> System.out.println("1");
-            case "2" -> System.out.println("2");
+            case "2" -> musicDescription();
             case "3" -> System.out.println("3");
             case "4" -> System.out.println("4");
-            case "5" -> System.out.println("5");
+            case "5" -> System.exit(0);
         }
         System.out.print("\n");
+        }
+    }
+        public void musicDescription(){
+        Scanner scanName = new Scanner(System.in);
+        System.out.println("Enter the music name : ");
+        String name = scanName.nextLine();
 
+        Scanner scanAuthor = new Scanner(System.in);
+        System.out.println("Enter the music author : ");
+        String author = scanAuthor.nextLine();
+
+        Scanner scanTime = new Scanner(System.in);
+        System.out.println("Enter the music duration : ");
+        String time= scanTime.nextLine();
+
+        Music music = new Music(name,author,time);
     }
 }
