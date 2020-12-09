a7dde6495dcd831d2205e0433180669dbdee03c0
Infernaton <lubin.pauly@gmail.com>
2020-12-09 16:01:03 +0100

    Update the Main menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,25 @@
 package com.company;
 
-import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        MenuMusic menu = new MenuMusic();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome !\n");
+        System.out.print("What do you want to do ?\n"
+                    +"1/ Some music ?\n"
+                    +"2/ Or just a game ?\n"
+                    +"3/ Quit."
+                    +"Selection : "
+                );
+        String choice = sc.next();
+        switch (choice) {
+            case "1" -> new MenuMusic();
+            case "2" -> new GameMenu();
+            case "3" -> System.exit(0);
+            default -> System.out.println("I don't understand your choice. Choose before Christmas !");
+        }
     }
 }
