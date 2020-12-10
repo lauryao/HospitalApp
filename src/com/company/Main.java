25b2ca3088608392094b07107f259978c640189a
Thomas Cardoso <72390135+tcardo06@users.noreply.github.com>
2020-12-10 11:45:11 +0100

    add : throws FileNotFoundException

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome !\n");
