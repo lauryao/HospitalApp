c99ca6002219dd128d1b3a99fc42e7bb1f142133
Thomas Cardoso <72390135+tcardo06@users.noreply.github.com>
2020-12-10 14:46:21 +0100

    throws IOException

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome !\n");
